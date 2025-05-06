93c9b594cbf1a5b3a5fab35f881c81598197d44b
DANIEL <daniel@ballendat.net>
2025-05-06 16:55:17 +0700

    Set base path to /static/ in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/static/', // Pfad für Assets anpassen, damit sie mit Django's STATIC_URL übereinstimmen
   plugins: [react()],
 })
